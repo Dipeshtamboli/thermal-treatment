e0d59d9ea7d73a79ebe82a4ea3582f636a3b8e2d
Dipesh Tamboli <dipeshtamboli@gmail.com>
2022-07-21 18:13:17 -0400

    clean with mu +- 2*std

--- clean_data.py
+++ clean_data.py
@@ -9,41 +9,45 @@ df_init = pd.read_excel(file_errors_location)
 temps = [225, 250, 275, 300, 325]
 times = [10, 20, 30, 40, 50]
 
+# times = [10]
+# temps = [300]
 clean_data = pd.DataFrame(columns=df_init.columns)
 
 for temp in temps:
     for time in times:
-        df = pd.read_excel(file_errors_location, sheet_name=f'{temp}-{time}')
-        # pdb.set_trace()
+        file_name = f'{temp}-{time}'
+        print(file_name)
+        df = pd.read_excel(file_errors_location, sheet_name=file_name)
 
-        # df['Point_No']
         # df = df[df['Point_No']>4 and df['Point_No']<= 20]
         df = df[df['Point_No']<= 20]
         df = df[df['Point_No']>4]
-        clean_data = pd.concat([clean_data, df], axis=0)
         
-        # print(df2.columns[4], df2.columns[7])
-        # after = df[df.columns[7]]
-        # before = df[df.columns[4]]
-        # k = (after-before)/before
-            
+        L_after = df[df.columns[7]]
+        L_before = df[df.columns[4]]
+        a_after = df[df.columns[8]]
+        a_before = df[df.columns[5]]
+        b_after = df[df.columns[9]]
+        b_before = df[df.columns[6]]
+
+        color_diff = np.sqrt((L_after-L_before)**2 + (a_after-a_before)**2 + (b_after-b_before)**2)
+        mean = np.mean(color_diff)
+        std = np.std(color_diff)
+
+        # print(f"before shape: {df.shape}")
+        df = df[color_diff < mean+2*std]
+        df = df[color_diff > mean-2*std]
+        # print(f"after shape: {df.shape}")
+        clean_data = pd.concat([clean_data, df], axis=0)
 
 clean_data.to_csv('Data4Dipesh/Ash/Ash_Front_Clean.csv')
 
-# plot two plots in one figure
-first_few = 72
-# pdb.set_trace()
-mean_L = np.mean([df[df2.columns[4]][:24], df[df2.columns[4]][24:48], df[df2.columns[4]][48:72]], axis=0)
-L_diff = df[df2.columns[4]] - df[df2.columns[7]]
+# first_few = 72
 
-fig, ax = plt.subplots(3, 1, figsize=(10, 10))
-ax[0].plot(df[df2.columns[4]][:first_few])
-ax[0].plot(mean_L)
-ax[1].plot(L_diff[:first_few])
-ax[2].plot(k[:first_few])
-plt.savefig('plots/Ash_Back.png')
-# plt.show()
+# fig, ax = plt.subplots(3, 1, figsize=(10, 10))
+# ax[0].plot(df[df2.columns[4]][:first_few])
+# ax[0].plot(mean_L)
+# ax[1].plot(L_diff[:first_few])
+# ax[2].plot(k[:first_few])
+# plt.savefig('plots/Ash_Back.png')
 
-
-# plt.plot(df[df2.columns[4]][:24])
-# plt.plot(L_diff[:24])
